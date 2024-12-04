c184d9bc29b07ff13318ec58aec3d25860b15802
ldevdantesl <buzurg.berdiev@icloud.com>
2024-12-04 18:58:16 -0500

    Initial Commit

---
+++ AimIt/App/AimItApp.swift
@@ -0,0 +1,17 @@
+//
+//  AimItApp.swift
+//  AimIt
+//
+//  Created by Buzurg Rakhimzoda on 4.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct AimItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
---
+++ AimIt/Presentation/Views/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  AimIt
+//
+//  Created by Buzurg Rakhimzoda on 4.12.2024.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
